bf5c8202e464ce5a9eeef11c45742f9268dc1c46
mainnet-pat <mainnet_pat@protonmail.com>
2022-07-02 11:35:29 +0000

    Add ens support on smartbch and amber

--- packages/dapp/src/hooks/useENS.js
+++ packages/dapp/src/hooks/useENS.js
@@ -1,16 +1,29 @@
+import { useWeb3Context } from 'contexts/Web3Context';
 import { ethers } from 'ethers';
 import { logError } from 'lib/helpers';
 import { getEthersProvider } from 'lib/providers';
 import { useEffect, useState } from 'react';
 
+const extraEnsAddressMap = {
+  10000: '0xCfb86556760d03942EBf1ba88a9870e67D77b627',
+  10001: '0x32f1FBE59D771bdB7FB247FE97A635f50659202b',
+};
+
 export function useENS(address) {
   const [ensName, setENSName] = useState('');
+  const { providerChainId } = useWeb3Context();
 
   useEffect(() => {
     async function resolveENS() {
       try {
         if (ethers.utils.isAddress(address)) {
-          const provider = await getEthersProvider(1);
+          const provider = await getEthersProvider(providerChainId);
+          const network = await provider.getNetwork();
+          const ensAddress = extraEnsAddressMap[providerChainId];
+          if (!network?.ensAddress && ensAddress) {
+            provider._network.ensAddress = ensAddress;
+          }
+
           const name = await provider.lookupAddress(address);
           if (name) setENSName(name);
         }
@@ -19,7 +32,7 @@ export function useENS(address) {
       }
     }
     resolveENS();
-  }, [address]);
+  }, [address, providerChainId]);
 
   return { ensName };
 }
